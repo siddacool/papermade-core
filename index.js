6c2f963197782858f89cb89713f3f6e3238a9a0f
Siddhesh Mangela <sidthecool007@gmail.com>
2021-10-26 11:01:45 +0530

    build removed

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import sass from "sass";
 import { promisify } from "util";
-import { writeFile, existsSync, mkdirSync } from "fs";
+import { writeFile } from "fs";
 const writeFilePromise = promisify(writeFile);
 
 async function main() {
-  if (!existsSync("build")) {
-    mkdirSync("build");
-  }
-
   const styleResult = await sass.renderSync({
     file: `${process.cwd()}/src/scss/index.scss`,
-    outFile: `${process.cwd()}/build/styles.css`,
+    outFile: "styles.css",
     outputStyle: "compressed",
   });
 
-  await writeFilePromise("build/styles.css", styleResult.css, "utf8");
+  await writeFilePromise("styles.css", styleResult.css, "utf8");
 }
 
 main();
