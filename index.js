c6bfeb07e64a735e3c8689ba0febc03adbe76840
Siddhesh Mangela <sidthecool007@gmail.com>
2021-10-26 10:54:56 +0530

    v 2 added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("test");
+import sass from "sass";
+import { promisify } from "util";
+import { writeFile, existsSync, mkdirSync } from "fs";
+const writeFilePromise = promisify(writeFile);
+
+async function main() {
+  if (!existsSync("build")) {
+    mkdirSync("build");
+  }
+
+  const styleResult = await sass.renderSync({
+    file: `${process.cwd()}/src/scss/index.scss`,
+    outFile: `${process.cwd()}/build/styles.css`,
+    outputStyle: "compressed",
+  });
+
+  await writeFilePromise("build/styles.css", styleResult.css, "utf8");
+}
+
+main();
